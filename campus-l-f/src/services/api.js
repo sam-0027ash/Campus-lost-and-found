59587d338e298e3f095910e9e0aa853a8049d7da
sam-0027ash <shamitha.bnh@gmail.com>
2026-05-10 20:06:44 +0530

    Fixed API route

--- campus-l-f/src/services/api.js
+++ campus-l-f/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://campus-lost-and-found-ifg7.onrender.com"
+  baseURL: "https://campus-lost-and-found-ifg7.onrender.com/api"
 });
 
 export default API;
